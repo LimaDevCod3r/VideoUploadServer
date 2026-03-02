de65f73e1a7ed3f0bca53fb0dce19801b2f530c8
Diego Lima <diegolima20100@gmail.com>
2026-03-02 05:31:43 -0300

    feat: setup project with Kestrel, FormOptions and FFmpeg config

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using FFMpegCore;
+using Microsoft.AspNetCore.Http.Features;
+using VideoUploadServer.Middlewares;
+using VideoUploadServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar caminho do FFmpeg
+GlobalFFOptions.Configure(options =>
+{
+    options.BinaryFolder = @"C:\ffmpeg\bin";
+});
+
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Configura o limite do Kestrel (servidor)
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = long.MaxValue;
+
+});
+
+// Configura o limite do Form
+builder.Services.Configure<FormOptions>(options =>
+{
+
+    options.MultipartBodyLengthLimit = long.MaxValue;
+});
+
+builder.Services.AddControllers();
+
+// Service
+builder.Services.AddScoped<UploadService>();
+var app = builder.Build();
+
+app.UseMiddleware<ApiKeyMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
