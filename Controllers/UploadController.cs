e433ebecfe8bd8ede3dfc6f7be75c7d6a1c436b4
Diego Lima <diegolima20100@gmail.com>
2026-03-02 05:32:26 -0300

    feat: add UploadController with GET, POST and DELETE endpoints

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using VideoUploadServer.Services;
+
+namespace VideoUploadServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController : ControllerBase
+{
+
+    private readonly UploadService _uploadService;
+
+
+    public UploadController(UploadService uploadService)
+    {
+        _uploadService = uploadService;
+    }
+
+
+    [HttpGet]
+    public IActionResult GetAllVideos()
+    {
+        var result = _uploadService.GetAllVideos();
+        return Ok(result);
+    }
+
+    [HttpGet("{fileName}")]
+    public IActionResult GetVideoByName(string fileName)
+    {
+        var result = _uploadService.GetVideoByName(fileName);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadVideos([FromForm] List<IFormFile> videos)
+    {
+        var result = await _uploadService.UploadVideos(videos);
+        return Ok(result);
+    }
+
+    [HttpDelete("{fileName}")]
+    public IActionResult DeleteVideo(string fileName)
+    {
+        var result = _uploadService.DeleteVideo(fileName);
+
+        if (!result)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+
+    }
+}
